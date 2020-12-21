34574039078d4723760080f1548a37748ac33952
tom42 <tom42@github.com>
2020-12-21 20:14:36 +0100

    getopt.c: removed gettext support altogether. Include private getopt headers.

--- src/getopt.c
+++ src/getopt.c
@@ -68,8 +68,9 @@
 #ifdef _LIBC
 # include <libintl.h>
 #else
-# include "gettext.h"
-# define _(msgid) gettext (msgid)
+/*# include "gettext.h"
+# define _(msgid) gettext (msgid)*/
+# define _(msgid) msgid
 #endif
 
 #if defined _LIBC
@@ -94,8 +95,8 @@
    GNU application programs can use a third alternative mode in which
    they can distinguish the relative order of options and other arguments.  */
 
-#include "getopt.h"
-#include "getopt_int.h"
+#include "argp-getopt.h"
+#include "argp-getopt_int.h"
 
 /* For communication from `getopt' to the caller.
    When `getopt' finds an option that takes an argument,
