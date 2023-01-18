eb3e3c4287a6ffd80291af5d122953a83308f289
tom42 <tom42@users.noreply.github.com>
2023-01-18 09:46:13 +0100

    Added SPDX license ID and copyright to argp-compat.c and argp-compat.h. These do not come from glibc but are written by me, so I'll change the copyright message as I see fit.

--- src/argp-compat.h
+++ src/argp-compat.h
@@ -1,20 +1,7 @@
 /*
+ * SPDX-FileCopyrightText: 2020 Thomas Mathys
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  * argp-standalone: standalone version of glibc's argp functions.
- * Copyright (C) 2020 Thomas Mathys
- *
- * argp-standalone is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * argp-standalone is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with the GNU C Library; if not, see
- * <http://www.gnu.org/licenses/>.
  */
 
 #ifndef ARGP_COMPAT_H_INCLUDED
