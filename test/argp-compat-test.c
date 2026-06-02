5f1490b0fa6bd8a68f66791d7728246bba1bb4db
tom42 <tom42@users.noreply.github.com>
2026-06-02 07:23:47 +0200

    Move __srgp_short_program_name test to its own function
    We're going to add more functions.

--- test/argp-compat-test.c
+++ test/argp-compat-test.c
@@ -8,8 +8,13 @@
 #include <string.h>
 #include "argp-namefrob.h"
 
-int main(int argc, char** argv)
+static void test__argp_short_program_name(char** argv)
 {
   assert(strcmp(__argp_short_program_name(), __argp_basename(argv[0])) == 0);
+}
+
+int main(int argc, char** argv)
+{
+  test__argp_short_program_name(argv);
   return 0;
 }
