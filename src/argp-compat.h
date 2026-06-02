611e730f8513f03e56431c2f76cd6954e0092031
tom42 <tom42@users.noreply.github.com>
2026-06-02 07:26:54 +0200

    Set up test for argp_compat_strerror()

--- src/argp-compat.h
+++ src/argp-compat.h
@@ -56,4 +56,8 @@ char* argp_compat_strndup(const char* s, size_t n);
 #define strndup argp_compat_strndup
 #endif
 
+// TODO: do we provide this unconditionally? Probably, yes?
+// TODO: use this from argp (either by hacking source or using preprocessor redirection)
+char* argp_compat_strerror(int errnum, char buf[], size_t size);
+
 #endif
--- test/argp-compat-test.c
+++ test/argp-compat-test.c
@@ -6,6 +6,7 @@
 #undef NDEBUG
 #include <assert.h>
 #include <string.h>
+#include "argp-compat.h"
 #include "argp-namefrob.h"
 
 static void test__argp_short_program_name(char** argv)
@@ -13,8 +14,17 @@ static void test__argp_short_program_name(char** argv)
   assert(strcmp(__argp_short_program_name(), __argp_basename(argv[0])) == 0);
 }
 
+static void test_argp_compat_strerror()
+{
+	// TODO: get access to argp_compat_strerror somehow
+	// TODO: pass correct args: is the buf/sizeof(buf) thing correct?
+	char buf[200];
+	assert(strcmp(argp_compat_strerror(EACCES, buf, sizeof(buf)), "") == 0);
+}
+
 int main(int argc, char** argv)
 {
   test__argp_short_program_name(argv);
+  test_argp_compat_strerror();
   return 0;
 }
