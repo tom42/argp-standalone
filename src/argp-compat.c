c9ec9f7c2533c0063981761201c86c53888cea73
tom42 <tom42@users.noreply.github.com>
2026-06-03 08:20:48 +0200

    Integrate unity

--- src/argp-compat.c
+++ src/argp-compat.c
@@ -81,6 +81,7 @@ char* argp_compat_strndup(const char* s, size_t n)
 // TODO: replace BSD and strerror path with this, one way or another
 // TODO: ensure _GNU_SOURCE is not defined here so we get the BSD variant
 // TODO: should we check for the BSD function instead in CMakeLists?
+// TODO: remember to also fix all items in argp-compat.h
 char* argp_compat_strerror(int errnum, char buf[], size_t size)
 {
   return strerror(errnum);
--- test/argp-compat-test.c
+++ test/argp-compat-test.c
@@ -3,28 +3,32 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  * argp-standalone - standalone version of glibc's argp functions.
  */
-#undef NDEBUG
-#include <assert.h>
-#include <string.h>
+#include <errno.h>
 #include "argp-compat.h"
 #include "argp-namefrob.h"
+#include "unity.h"
 
-static void test__argp_short_program_name(char** argv)
+void setUp() {}
+void tearDown() {}
+
+static char** g_argv;
+
+void test__argp_short_program_name(void)
 {
-  assert(strcmp(__argp_short_program_name(), __argp_basename(argv[0])) == 0);
+  TEST_ASSERT_EQUAL_STRING(__argp_basename(g_argv[0]), __argp_short_program_name());
 }
 
-static void test_argp_compat_strerror()
+void test_argp_compat_strerror(void)
 {
-  // TODO: pass correct args: is the buf/sizeof(buf) thing correct?
-  // TODO: awesome: it already gets unwieldy: can we source a tiny unit test framework for C with better assertion messages?
   char buf[200];
-  assert(strcmp(argp_compat_strerror(EACCES, buf, sizeof(buf)), "") == 0);
+  TEST_ASSERT_EQUAL_STRING("Permission denied", argp_compat_strerror(EACCES, buf, sizeof(buf)));
 }
 
 int main(int argc, char** argv)
 {
-  test__argp_short_program_name(argv);
-  test_argp_compat_strerror();
-  return 0;
+  g_argv = argv;
+  UNITY_BEGIN();
+  RUN_TEST(test__argp_short_program_name);
+  RUN_TEST(test_argp_compat_strerror);
+  return UNITY_END();
 }
