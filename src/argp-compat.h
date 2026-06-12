66e12877adc61f9330aa948ad98b468791d7c678
tom42 <tom42@users.noreply.github.com>
2026-06-12 19:58:09 +0200

    Remove duplicate license tag [skip ci]

--- src/argp-compat.h
+++ src/argp-compat.h
@@ -1,4 +1,3 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
 /* SPDX-FileCopyrightText: 2020 Thomas Mathys
    SPDX-License-Identifier: LGPL-2.1-or-later
    argp-standalone - standalone version of glibc's argp functions. */
