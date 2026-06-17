5359bea5e8bcdd247f4397c7d7a38fa86ad93631
tom42 <tom42@users.noreply.github.com>
2026-06-17 07:28:41 +0200

    Undo most changes done to argp-test.c
    We will now pull in rand via test_support.h

--- test/argp-test.c
+++ test/argp-test.c
@@ -17,10 +17,6 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE 1
-#endif
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -30,40 +26,7 @@
 #include <string.h>
 #include <argp.h>
 
-// TODO: move random() and (x)asprintf into some test support header/library. I do not wish to see these here all the time
-/*
- * Implementation of random() for systems that don't have it.
- * Suitable for test purposes only, not for production code.
- */
-#if defined(HAVE_RANDOM) && !HAVE_RANDOM
-int random(void)
-{
-  return rand();
-}
-#endif
-
-/*
- * Implementation of asprintf() for systems that don't have it.
- * Suitable for test purposes only, not for production code.
- */
-#if defined(HAVE_ASPRINTF) && !HAVE_ASPRINTF
-#include <stdarg.h>
-void asprintf(char** strp, const char* fmt, ...)
-{
-  const size_t bufsize = 1024;
-  va_list ap;
-  va_start(ap, fmt);
-
-  /* Since this is test code we don't bother checking whether malloc returns 0. */
-  *strp = malloc(bufsize);
-
-  /* Format text and ensure it is terminated in any case. */
-  vsnprintf(*strp, bufsize, fmt, ap);
-  (*strp)[bufsize - 1] = 0;
-
-  va_end(ap);
-}
-#endif
+#include "test_support.h"
 
 const char *argp_program_version = "argp-test 1.0";
 
@@ -218,12 +181,12 @@ help_filter (int key, const char *text, void *input)
   if (key == ARGP_KEY_HELP_POST_DOC && text)
     {
       time_t now = time (0);
-      asprintf(&new_text, text, ctime(&now)); // TODO: changed this to use asprintf not xasprintf
+      new_text = xasprintf (text, ctime (&now));
     }
   else if (key == 'f')
     /* Show the default for the --foonly option.  */
-    asprintf (&new_text, "%s (ZOT defaults to %x)", // TODO: changed this to use asprintf not xasprintf
-	      text, params->foonly_default);
+    new_text = xasprintf ("%s (ZOT defaults to %x)",
+		          text, params->foonly_default);
   else
     new_text = (char *)text;
 
---
+++ test/test_support.h
@@ -0,0 +1,64 @@
+/* SPDX-FileCopyrightText: 2020 Thomas Mathys
+   SPDX-License-Identifier: LGPL-2.1-or-later
+   argp-standalone - standalone version of glibc's argp functions. */
+
+#ifndef ARGP_TEST_SUPPORT_H_INCLUDED
+#define ARGP_TEST_SUPPORT_H_INCLUDED
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+
+/* Implementation of random() for systems that don't have it.
+ * Suitable for test purposes only, not for production code. */
+#if defined(HAVE_RANDOM) && !HAVE_RANDOM
+static int random(void)
+{
+  return rand();
+}
+#endif
+
+/* Implementation of xasprintf() for systems that don't have it.
+ * Suitable for test purposes only, not for production code. */
+#if defined(HAVE_XASPRINTF) && !HAVE_XASPRINTF
+static char* xasprintf(const char* fmt, ...)
+{
+  // TODO: this is tricky. Review/debug
+
+  /* Measure length of resulting string. */
+  va_list ap;
+  va_start(ap, fmt);
+  int slen = vsnprintf(NULL, 0, fmt, ap); // TODO: this may actually fail with a negative value. Should handle that (errno is set in that case)
+  va_end(ap);
+
+  /* Calculate buffer size. The + 1 cannot overflow since slen is int. */
+  size_t bufsiz = (size_t)slen + 1;
+
+  /* Allocate memory, abort if insufficient memory. */
+  char* buf = malloc(bufsiz);
+  // TODO: abort if insufficient memorty
+
+  /* Print to buffer. */
+  va_start(ap, fmt);
+  // TODO: what to pass here, bufsiz or slen?
+  vsnprintf(buf, slen, fmt, ap); // TODO: again, this may fail, although it should not. Anyway, handle it for the sake of completeness
+  // TODO: is buf terminated in any case or do we have to do this ourselves?
+  va_end(ap);
+
+  return buf;
+
+	// TODO: below is test code, redo this
+#if 0
+		  /* Format text and ensure it is terminated in any case. */
+		vsnprintf(strp, bufsize, fmt, ap);
+	strp[bufsize - 1] = 0;
+	
+		
+		return strp; // This should contain "Glork a foonly (ZOT defaults to 29)" (35 characters excluding the terminating zero)
+#endif
+}
+#endif
+
+#endif
