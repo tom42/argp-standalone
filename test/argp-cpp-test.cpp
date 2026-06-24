731bafce55e2800fb954f891e14826f989eba639
tom42 <tom42@users.noreply.github.com>
2026-06-24 22:06:08 +0200

    Add C++ compilation test

---
+++ test/argp-cpp-test.cpp
@@ -0,0 +1,11 @@
+/* SPDX-FileCopyrightText: 2026 Thomas Mathys
+   SPDX-License-Identifier: LGPL-2.1-or-later
+   argp-standalone - standalone version of glibc's argp functions. */
+
+#include <argp.h>
+
+int main()
+{
+  // TODO: do something slightly more useful? (test all headers?)
+  argp_failure(nullptr, 0, 0, "failure");
+}
