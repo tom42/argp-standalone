7568fe68b99a6d6327dcd5e641e0fa0105ce68f8
tom42 <tom42@users.noreply.github.com>
2026-06-29 21:44:08 +0200

    Explain what this test does and remove remaining TODO items.

--- test/argp-cpp-test.cpp
+++ test/argp-cpp-test.cpp
@@ -2,6 +2,9 @@
    SPDX-License-Identifier: LGPL-2.1-or-later
    argp-standalone - standalone version of glibc's argp functions. */
 
+// C++ compilation and linking test.
+// The code doesn't do anything actually useful.
+
 #include <argp.h>
 #include <argp-fmtstream.h>
 #include <argp-getopt.h>
@@ -9,9 +12,8 @@
 
 int main()
 {
-  // TODO: do something slightly more useful? (test all headers? actually no, other headers are not public)
   argp_failure(nullptr, 0, 0, "failure message");
   argp_fmtstream_free(argp_make_fmtstream(stdout, 0, 0, 0));
-  getopt(0, nullptr, nullptr); // TODO: real args, verify return code?
-  getopt_long(0, nullptr, nullptr, nullptr, nullptr); // TODO: real args, verify return code?
+  getopt(0, nullptr, nullptr);
+  getopt_long(0, nullptr, nullptr, nullptr, nullptr);
 }
