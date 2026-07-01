827f8cb5bf4ac9127727727d6ed1dbdeb14a780a
tom42 <tom42@users.noreply.github.com>
2026-07-01 22:19:38 +0200

    Test

--- test/tst-ldbl-argp.c
+++ test/tst-ldbl-argp.c
@@ -61,5 +61,5 @@ main(int argc, char** argv)
   //int remaining;
   //argv[0] = "test-argp";
   //argp_parse (&argp, argc, argv, 0, &remaining, NULL);
-  printf("%Lf%f%Lf%f\n", (long double) -1, (double) -2, (long double) -3, (double) -4);
+  argp_error(NULL, "%Lf%f%Lf%f\n", (long double) -1, (double) -2, (long double) -3, (double) -4);
 }
