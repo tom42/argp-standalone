638561e7923b59d574d50a1c1abb19f71d395558
tom42 <tom42@users.noreply.github.com>
2026-07-23 22:02:26 +0200

    Annotated argp-getopt_core.h

--- src/argp-getopt_core.h
+++ src/argp-getopt_core.h
@@ -26,6 +26,7 @@
    a protective #error, because the guard macro for getopt.h in
    gnulib is not fixed.  */
 
+/* argp-standalone: we do not have __BEGIN_DECLS/__END_DECLS. */
 #ifdef  __cplusplus
 extern "C" {
 #endif
