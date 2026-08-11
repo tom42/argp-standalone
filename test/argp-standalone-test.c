41b75714315e8a9f7cac3feea8013288afc977a9
tom42 <tom42@users.noreply.github.com>
2026-08-11 20:37:19 +0200

    Make argp-standalone-test (ex argp-failure-test) more generic

--- test/argp-standalone-test.c
+++ test/argp-standalone-test.c
@@ -2,17 +2,35 @@
    SPDX-License-Identifier: LGPL-2.1-or-later
    argp-standalone - standalone version of glibc's argp functions. */
 
-/* Smoke test our strerror replacement by calling into argp_failure.
-   Print an error message which then needs to be checked e.g. by ctest. */
-
 #include <stdlib.h>
 #include "argp.h"
 
+static const char doc[] = "Various tests for argp-standalone, to be run with CTest";
+
+#define OPT_ARGP_FAILURE 256
+
+static struct argp_option options[] =
+{
+  { "argp-failure", OPT_ARGP_FAILURE, NULL, 0, "Smoke test the strerror replacement by calling into argp_failure. This will print an error message which needs to be checked e.g. by CTest."},
+  { 0 }
+};
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state)
+{
+  switch (key)
+  {
+    case OPT_ARGP_FAILURE:
+      argp_failure(state, 0, EACCES, "Error message format string");
+      return 0;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+}
+
+static struct argp argp = { options, parse_opt, 0, doc };
+
 int main(int argc, char** argv)
 {
-  struct argp_state* state = NULL; /* argp_failure can work without state. */
-  int status = 0; /* Keep argp_failure from calling exit. */
-
-  argp_failure(state, status, EACCES , "Error message format string");
+  argp_parse(&argp, argc, argv, 0, 0, 0);
   return EXIT_SUCCESS;
 }
